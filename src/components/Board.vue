0045d1d921f22d82dd81b9e3c7268a72e728474a
이주영 <ijuyeong@ijuyeong-ui-MacBookPro.local>
2022-01-15 14:16:44 +0900

    feat: 동적, 중첩 라우팅 추가

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,6 +2,9 @@
   <div>
     Board
     <div>bid: {{ bid }}</div>
+    <hr />
+    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+    <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
   </div>
 </template>
 
