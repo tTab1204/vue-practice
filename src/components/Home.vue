e2215fb0b22264de75d91350ae22273f30f721a3
이주영 <ijuyeong@ijuyeong-ui-MacBookPro.local>
2022-01-17 19:51:53 +0900

    feat: Board list 추가

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,25 @@
 <template>
   <div>
-    Home
-    <div>
-      Board List:
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        <div v-for="b in boards" :key="b.id">
-          {{ boards }}
-        </div>
+    <div class="home-title">Personal Boards</div>
+    <div class="board-list">
+      <div
+        class="board-item"
+        v-for="b in boards"
+        :key="b.id"
+        :data-bgcolor="b.bgColor"
+        ref="boardItem"
+      >
+        <router-link :to="`/b/${b.id}`">
+          <div class="board-item-title">{{ b.title }}</div>
+        </router-link>
       </div>
-      <ul>
-        <li>
-          <router-link to="/b/1">Board 1</router-link>
-        </li>
-        <li>
-          <router-link to="/b/2">Board 2</router-link>
-        </li>
-      </ul>
+      <div class="board-item">
+        <a class="new-board-btn" href="" @click.prevent="addBoard"
+          >Create new board...</a
+        >
+      </div>
     </div>
+    <!-- <add-board v-if="isAddBoard"></add-board> -->
   </div>
 </template>
 
@@ -28,22 +30,80 @@ export default {
   data() {
     return {
       loading: false,
-      boards: '',
+      boards: [],
+      error: '',
     };
   },
+
   created() {
     this.fetchData();
+  },
+
+  updated() {
+    this.$refs.boardItem.forEach((item) => {
+      item.style.backgroundColor = item.dataset.bgcolor;
+    });
   },
 
   methods: {
     async fetchData() {
       this.loading = true;
-      const data = await board.fetch();
-      this.boards = data;
+      try {
+        const data = await board.fetch();
+        this.boards = data.list;
+      } catch (error) {
+        console.log(error);
+      }
       this.loading = false;
+    },
+    addBoard() {
+      console.log('addBoard!!');
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+.home-title {
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.board-list {
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.board-item {
+  width: 23%;
+  height: 100px;
+  margin: 0 2% 20px 0;
+  border-radius: 3px;
+}
+.board-item a {
+  text-decoration: none;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.board-item a:hover,
+.board-item a:focus {
+  background-color: rgba(0, 0, 0, 0.1);
+  color: #666;
+}
+.board-item-title {
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 10px;
+}
+.board-item a.new-board-btn {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  height: 100px;
+  width: inherit;
+  color: #888;
+  font-weight: 700;
+}
+</style>
